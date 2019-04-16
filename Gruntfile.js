009738d629a0ab62dc9069ee088ec6e9ec6fcf49
yangying1998 <1204749635@qq.com>
2019-04-16 16:09:12 +0800

    矩形计算器

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,107 @@
-/* global module: true */
+/*global module:true*/
 module.exports = function (grunt) {
-    grunt.initConfig({
-      eslint: {
-        options: {
-          configFile: '.eslintrc.json'
-        },
-        target: ['*.js']
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'  
       },
-      csslint: {
-        options: {
-          csslintrc: '.csslintrc'
-        },
-        src: '*.css'
+      target: ['*.js']  
+    },
+
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'  
       },
-      htmlhint: {
-        options: {
-          htmlhintrc: '.htmlhintrc'
-        },
-        src: '*.html'
+      src: '*.css'             
+    },
+
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'  
       },
-      mocha: {
-        test: {
-          src: ['test/index.html'],
-        },
-        options: {
-          run: true,
-          reporter: 'Spec'
-        }
+      src: '*.html'    
+    },
+    //添加单元测试代码 mocha chai
+    mocha:{
+      test:{
+        src:['text/index.html'],               
       },
-      htmlmin: {
-        options: {
-          collapseWhitespace: true,
-          preserveLineBreaks: false
-        },
+      options:{
+        run:true,
+        reporter:'Spec'                      
+      }    
+    },
+   //代码压缩构建
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false                        
+      },
+      files: {
+        src: 'dist/index.html',
+        dest: 'dist/index.html'                                                     
+      }          
+    },
+
+    cssmin: {
+      'dist/rectangle.css': 'rectangle.css'                          
+    },
+
+    uglify: {
+      release:{
         files: {
-          src: './index.html',
-          dest: 'dist/index.html'
-        }
+          'dist/bundle.min.js': 'dist/bundle.js',                                                 }                
+      }             
+    },
+
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+          dest: 'dist'                                                    
+      }                           
+    },
+
+    usemin: {
+      html: ['dist/index.html']                          
+    },
+
+    concat: {
+      options: {
+        separator: ';'                                    
       },
-      cssmin: {
-        'dist/rectangle.css': 'rectangle.css'
-      },
-      uglify: {
-        release:{
-          files: {
-            'dist/rectangle.js': 'rectangle.js',
-            'dist/calc.js': 'calc.js'
-          }
-        }
-      }
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-htmlhint');
-    grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-mocha');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-    grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
-    grunt.registerTask('unitTest', ['mocha']);
-    grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
-  };
+      js: {
+        src: ['rectangle.js', 'calc.js'],
+        dest: 'dist/bundle.js'                                               
+      }   
+    },
+
+    clean: ['dist/bundle.js', '.tmp'],
+    copy: {
+      html: {
+        src: './index.html',
+        dest: './dist/index.html'                                                         
+      }           
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  //v0.5
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+
+  grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('unitTest',['mocha']);
+
+  grunt.registerTask('release', ['copy', 'useminPrepare', 'concat','uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
+}
--- rectangle.js
+++ rectangle.js
@@ -1,18 +1,63 @@
-/* global Rectangle: true */
+/* global Rectangle, validate: true */
 $(function() {
-    var $width = $('#width'),
-        $height = $('#height'),
-        $btnCal = $('#calculate'),
-        $perimeter = $('#perimeter'),
-        $area = $('#area');
-  
-    $btnCal.click(function(){
-      var w = $width.val(),
-          h = $height.val();
-  
-      var r = new Rectangle(w, h);
-  
-      $perimeter.val(r.perimeter());
-      $area.val(r.area());
+    var $width=$('#width'),
+        $height=$('#height'),
+        $btnCal=$('#calculate'), 
+        $perimeter=$('#perimeter'),
+        $area=$('#area'),
+        $widthValidate = $('#width-validate'),
+        $heightValidate = $('#height-validate'),
+        isPassValidate = false;
+
+    $width.focusout(function() {
+      var result = validate($width.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+              $widthValidate.html('宽度' + result.reason);
+              $width.select();                           
+          } else {
+              $widthValidate.html('');                      
+          }           
     });
-  });
+
+    $height.focusout(function() {
+      var result = validate($height.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+              $heightValidate.html('高度' + result.reason);
+              $height.select();                  
+          } else {
+              $heightValidate.html('');                 
+          }
+    });
+
+    $btnCal.click(function(){
+          if(!isPassValidate) return;
+          var w = $width.val(),
+              h = $height.val();
+          var r = new Rectangle(w,h);
+          $perimeter.val(r.perimeter());
+          $area.val(r.area());
+              
+    });
+
+
+    function validate(field){
+      var $data = $(field),
+          $message = $(field + '-validate'),
+          label = $(field).attr('data-label');
+      if(!/^-?(0|[1-9]\d*)(\.\d)?([eE][+-]?\d+)?$/.test($data.val())){
+        $message.html(label+'必须是数值');
+        $data.select();
+        result false;
+      }
+      if(window.Number($data.val())<0){
+        $message.html(label+'必须大于0');
+        $data.select();
+        return false;
+      }
+      $message.html('');
+      return true;
+    }
+
+});
